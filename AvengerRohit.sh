92359a1290dff23733aa12d4bc2fc10794e718a3
Rohit Pathak <73642501+avengerrohit@users.noreply.github.com>
2021-01-06 15:17:37 +0530

    Add files via upload

---
+++ AvengerRohit.sh
@@ -0,0 +1,14 @@
+apt update && termux-setup-storage && apt upgrade -y
+apt install figlet -y
+apt install cowsay -y
+apt install ruby -y
+apt install wget -y
+apt install toilet -y
+wget https://github.com/busyloop/lolcat/archive/master.zip
+unzip master.zip
+cd lolcat-master/bin
+gem install lolcat
+
+echo This tool is created by Rohit Pathak.
+
+figlet Rohit Pathak | lolcat
